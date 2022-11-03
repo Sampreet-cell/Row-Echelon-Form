b95b61de55dd961c0ba9eb18643a38c78a94b283
SAMPREET DEY <116565203+Sampreet-cell@users.noreply.github.com>
2022-11-03 10:18:36 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,12 @@
+import os
+from row_echelon_form import *
+
+matrix=[[4,8,3],[5,-5,2],[3,-9,-6]]
+solution=REF(matrix)
+os.system("cls")
+print("Initial Matrix: ")
+solution.array()
+solution.sort_zero()
+solution.row_echelon_form()
+solution.reduced_row_echelon_form()
+solution.sort_zero()

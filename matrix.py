0f8e844aa1d32e25ff467c10355d044f0d35672e
SAMPREET DEY <116565203+Sampreet-cell@users.noreply.github.com>
2022-11-03 11:55:02 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import os
 from row_echelon_form import *
 
-matrix=[[4,8,3],[5,-5,2],[3,-9,-6]]   #<--Enter a square matrix
+matrix=[]   #<--Enter a square matrix, in the form of [1,2,3][4,5,6][7,8,9]
 solution=REF(matrix)
 os.system("cls")
 print("Initial Matrix: ")
